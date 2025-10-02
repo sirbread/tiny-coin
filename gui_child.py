c6602d569b35dae4d94421c3308525ece56a4d58
sirbread <gverma0824@outlook.com>
2025-10-02 02:06:52 -0600

    child panel working :DD

--- gui_child.py
+++ gui_child.py
@@ -61,6 +61,41 @@ class ChildApp(tk.Tk):
         self.current_file_path = None
         self.show_login_view()
 
+#child
+    def transfer_coins(self):
+        sender_coin = self.current_coin_obj
+        messagebox.showinfo("to recipient", "ask the person you're transferring TO to select THEIR wallet file.")
+        recipient_path = filedialog.askopenfilename(title="select RECIPIENT's .coin file", filetypes=[("tinycoin files", "*.coin")])
+        if not recipient_path: return
+        recipient_pass = simpledialog.askstring("recipient's password", "ask the person you're transferring TO to enter THEIR password:", show='*')
+        if not recipient_pass: return
+
+        recipient_coin = self.manager.read_coin_file(recipient_path)
+        if not (recipient_coin and recipient_coin.verify_child_password(recipient_pass)):
+            messagebox.showerror("error", "recipient wallet couldn't be opened, check password.")
+            return
+
+        if sender_coin.file_id == recipient_coin.file_id:
+            messagebox.showwarning("error", "good thinking, but you cannot transfer coins to your own wallet.")
+            return
+
+        try:
+            amount = simpledialog.askfloat("transfer amount", f"your balance: {sender_coin.balance:.2f}\nenter amount to transfer to {recipient_coin.owner_name}:")
+            if amount is None or amount <= 0:
+                messagebox.showwarning("invalid", "transfer amount must be positive")
+                return
+
+            success, message = perform_transfer(sender_coin, recipient_coin, amount)
+            if success:
+                self.manager.write_coin_file(self.current_file_path, sender_coin)
+                self.manager.write_coin_file(recipient_path, recipient_coin)
+                self.update_wallet_display()
+                messagebox.showinfo("sucesss", message)
+            else:
+                messagebox.showerror("transfer failed", message)
+        except Exception as e:
+            messagebox.showerror("error", f"an unexpected error occurred: {e}")
+
 
 
 if __name__ == "__main__":
